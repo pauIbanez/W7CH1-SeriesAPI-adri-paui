3638e5d90952a7b3336c2690c504d643a1dc8960
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-23 18:22:51 +0100

    added hashedPassword before creating user

--- src/server/controllers/usersControllers.js
+++ src/server/controllers/usersControllers.js
@@ -1,10 +1,19 @@
+const bcrypt = require("bcrypt");
 const User = require("../../database/models/User");
 
 const createUser = async (req, res, next) => {
-  const newUser = req.body;
+  const { name, username, password, admin } = req.body;
+  if (!name || !username || !password || !admin) {
+    const error = new Error("Not all fields are filled");
+    error.code = 400;
+    next(error);
+    return;
+  }
   try {
-    await User.create(newUser);
-    res.status(201).json(newUser);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await User.create({ name, username, hashedPassword, admin });
+    res.status(201).json({ name, username });
   } catch (error) {
     error.code = 409;
     error.message = "Username already taken";
