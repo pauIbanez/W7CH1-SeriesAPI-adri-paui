2e5fe13e98c3624e1005d6e13b478cab0a6006cf
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-23 20:52:38 +0100

    added auth middleware

---
+++ src/server/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const headerAuth = req.header("Authorization");
+  if (!headerAuth) {
+    const error = new Error("Token missing");
+    error.code = 401;
+    next(error);
+    return;
+  }
+  const token = headerAuth.replace("Bearer ", "");
+  try {
+    const { username, id } = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = {
+      username,
+      id,
+    };
+    next();
+  } catch (error) {
+    const eror = new Error("Wrong token");
+    eror.code = 401;
+    next(eror);
+  }
+};
+
+module.exports = { auth };
