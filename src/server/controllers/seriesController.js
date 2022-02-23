9608052c9fe9ba01b1b5aafe3f8a93a9cfb65362
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-23 19:55:01 +0100

    added error handling for listAllSeries

--- src/server/controllers/seriesController.js
+++ src/server/controllers/seriesController.js
@@ -3,12 +3,16 @@ const User = require("../../database/models/User");
 
 const listAllSeries = async (req, res, next) => {
   const { id } = req.user;
-
+  let errorMessage = "User not found";
   try {
     const user = await User.findById(id);
+    errorMessage = "Series not fkound in the database";
     const userSerires = await Serie.find({ id: { $in: user.series } });
     res.json({ series: userSerires });
   } catch (error) {
+    const newError = { ...error };
+    newError.code = 404;
+    newError.message = errorMessage;
     next(error);
   }
 };
