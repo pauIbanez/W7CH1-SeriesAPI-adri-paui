96d35141c8e89ed011ca30c1fd16cc01812ffd95
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-24 19:24:37 +0100

    added error handling to createplatform

--- src/server/controllers/platformsController.js
+++ src/server/controllers/platformsController.js
@@ -5,9 +5,15 @@ const getAllPlatforms = async (req, res) => {
   res.json({ platforms });
 };
 
-const createPlatform = async (req, res) => {
-  const newPlatform = req.body;
-  const createdPlatform = await Platform.create(newPlatform);
-  res.status(201).json(createdPlatform);
+const createPlatform = async (req, res, next) => {
+  try {
+    const newPlatform = req.body;
+    const createdPlatform = await Platform.create(newPlatform);
+    res.status(201).json(createdPlatform);
+  } catch (error) {
+    const newError = { ...error };
+    newError.code = 400;
+    next(error);
+  }
 };
 module.exports = { getAllPlatforms, createPlatform };
